82afc66ca466e2e14ef16a14d58fe8a7518bd3fd
FranciscodeJ <147544324+FranciscodeJ@users.noreply.github.com>
2023-10-20 14:18:11 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,10 +9,10 @@ namespace Práctica__3
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int num1 = Int32.Parse(textBox1.Text);
+            int num1 = Int32.Parse(textBox1.Text); // se declaró que en los valores que se escriban en las cajas de texto se asignen a num1 y2. 
             int num2 = Int32.Parse(textBox2.Text);
-            int sum = num1 + num2;
-            label3.Text = "Sum = " + sum;
+            int sum = num1 + num2;                 //se escribió una ecuación para obtenr la suma a aprtir de num 1 y 2.
+            label3.Text = "Sum = " + sum;         // se declaró que se pondrá en el cuadro para texto label el resultado, al lado del texto "Sum ="
         }
 
         private void label2_Click(object sender, EventArgs e)
